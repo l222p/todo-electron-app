3009bff1359890a834982de68f2992c89da5419f
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-04-10 14:02:47 -0600

    Update todo

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+/* eslint-disable no-unused-vars */
+
 const axios = require("axios");
 
 var todoList = document.getElementById("todo-list-id");
@@ -7,19 +8,28 @@ function getTodos() {
         .then(res => {
             console.log(res);
             var stringHtml = "";
-            res.data.map(x => { stringHtml += `<li class="todo"><span class="todo__content">${x.description}</span> <button onclick="deleteTodo(${x.id})">Delete</button></li>` })
+            res.data.map(x => { stringHtml += `<li class="${x.completed? "todo todo--completed": "todo"}" onclick="setCompleted(this, ${x.id}, ${x.completed})"><a class="filter__link" href="#" onclick="deleteTodo(event, ${x.id})"><i class="fa fa-times"></i></a> <span class="todo__content">${x.description}</span></li>` })
             todoList.innerHTML = stringHtml;
 
         })
 }
 
-function deleteTodo(id) {
+function deleteTodo(e, id) {
+    e.preventDefault();
     console.log("delete " + id);
     axios.delete("http://localhost:8080/todos/" + id)
         .then(() => getTodos())
 
 }
 
+function setCompleted(element, id, completed){
+    console.log("Completed " + element + id)
+    element.className = completed ? "todo": "todo todo--completed";
+    axios.put("http://localhost:8080/todos/" + id, {
+        completed: !completed,
+    }).then(() => getTodos());
+
+}
 function createTodo() {
     var todo = document.getElementById("todo");
     console.log("create " + todo.value);
